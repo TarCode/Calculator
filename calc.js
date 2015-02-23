dab3a0253788dd33c8b664265233ce3d2a8c195e
TarCode <taariq@projectcodex.co>
2015-02-23 16:28:28 +0200

    Calculator Project

---
+++ calc.js
@@ -0,0 +1,67 @@
+var calc = {
+	num1 : 0,
+	num2 : 0,
+
+	append : function(id){
+
+		switch(id){
+			case 'num1':
+				document.getElementById('displ').innerHTML += '1';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+			case 'num2':
+				document.getElementById('displ').innerHTML += '2';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+			case 'num3':
+				document.getElementById('displ').innerHTML += '3';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+			case 'num4':
+				document.getElementById('displ').innerHTML += '4';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+			case 'num5':
+				document.getElementById('displ').innerHTML += '5';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+			case 'num6':
+				document.getElementById('displ').innerHTML += '6';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+			case 'num7':
+				document.getElementById('displ').innerHTML += '7';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+			case 'num8':
+				document.getElementById('displ').innerHTML += '8';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+			case 'num9':
+				document.getElementById('displ').innerHTML += '9';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+			case 'num0':
+				document.getElementById('displ').innerHTML += '0';
+				num1 = document.getElementById(id).innerHTML;
+			break;
+
+		}
+
+	}
+	
+}
+
+function add(){
+
+
+}
